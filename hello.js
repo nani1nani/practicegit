ab18f4ed34b966b584b2b689e2b7b741225999fd
nani1nani <nani121323@gmail.com>
2025-03-16 19:15:27 +0530

    added some extra features

---
+++ hello.js
@@ -0,0 +1,20 @@
+const a = document.getElementById("root");
+const b = document.createElement("h1");
+b.innerHTML = "hello";
+a.appendChild(b);
+const button = document.createElement("button");
+button.innerHTML = "click";
+a.appendChild(button);
+
+const box = document.createElement("div");
+
+a.appendChild(box);
+box.classList.add("custom-box");
+
+button.addEventListener("click", (e) => {
+  // Step 3: Set inline styles
+  box.style.width = "100px";
+  box.style.height = "100px";
+  box.style.backgroundColor = "blue";
+  box.style.margin = "10px";
+});
